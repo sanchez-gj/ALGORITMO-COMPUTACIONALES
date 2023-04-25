0896e5914ccde6e97ab686c8264eb7b0583259e9
jsoos <pr75505254@zegelipae.pe>
2023-04-25 08:05:49 -0500

    variable textos

--- sem3.py
+++ sem3.py
@@ -1,2 +1,6 @@
 print("mi", "ejercicio", "para",sep="*",end="*\n")
 print("impresiones")
+#-----------------------------------------
+#BIENVENIDO A PYTHON
+print("FUNDAMENTOS DE PROGRAMACION")
+nombre = "LORD PATRICIO"
